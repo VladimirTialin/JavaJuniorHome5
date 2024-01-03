0bbd8bbb71e4842076d319af44cde513dfc624c8
Vladimir <wel.tall@yandex.ru>
2024-01-03 18:28:47 +0300

    finish 1

---
+++ src/main/java/ru/gb/lesson5/Presenter/Client2.java
@@ -0,0 +1,9 @@
+package ru.gb.lesson5.Presenter;
+
+import ru.gb.lesson5.Model.Client;
+
+public class Client2 {
+    public static void main(String[] args) {
+        new Client().start();
+    }
+}
